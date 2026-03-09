031ceec043659c2a3f363c62880998da679412e9
AwaisKhanz <mawais2k2@gmail.com>
2026-03-09 06:58:37 +0500

    Normalize user account query filters

--- apps/api/src/users/dto/user.dto.ts
+++ apps/api/src/users/dto/user.dto.ts
@@ -1,3 +1,4 @@
+import { Transform } from 'class-transformer';
 import {
   IsBoolean,
   IsEmail,
@@ -9,6 +10,7 @@ import {
 } from 'class-validator';
 import { Role } from '@tbms/shared-types';
 import { PaginationQueryDto } from '../../common/dto/pagination-query.dto';
+import { transformOptionalString } from '../../common/dto/query-transformers';
 import { IsCuidString } from '../../common/validators/is-cuid-string';
 
 export class CreateUserDto {
@@ -65,10 +67,12 @@ export class SetUserActiveDto {
 
 export class ListUsersQueryDto extends PaginationQueryDto {
   @IsOptional()
+  @Transform(transformOptionalString)
   @IsEnum(Role)
   role?: Role;
 
   @IsOptional()
+  @Transform(transformOptionalString)
   @IsString()
   @IsCuidString()
   branchId?: string;
